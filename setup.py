60ca76325b39b48162a9d54850614bff4e2e7734
David Wolever <david@wolever.net>
2013-05-19 02:26:45 -0400

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
 setup(
     name="nose-parameterized",
-    version="0.2",
+    version="0.3",
     url="https://github.com/wolever/nose-parameterized",
     author="David Wolever",
     author_email="david@wolever.net",
-    description="Nose decorator for parameterized testing",
+    description="Decorator for parameterized testing with Nose",
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 3'
