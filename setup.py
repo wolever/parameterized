e21db77ac0b626fbde9db16050ea271526d8f3b8
David Wolever <david@wolever.net>
2014-11-05 22:33:28 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
 setup(
     name="nose-parameterized",
-    version="0.3.4",
+    version="0.3.5",
     url="https://github.com/wolever/nose-parameterized",
     author="David Wolever",
     author_email="david@wolever.net",
