173e499876fba0f216693a13a664403a71163bdc
David Wolever <david@wolever.net>
2017-03-21 13:31:52 -0400

    Update misspelling helper

--- misspelling-helper/nose-parametrized/setup.py
+++ misspelling-helper/nose-parametrized/setup.py
@@ -3,9 +3,9 @@ from distutils.core import setup
 
 if __name__ == "__main__":
     if "sdist" not in sys.argv[1:]:
-        raise ValueError("please use the 'nose-parameterized' pypi package instead of 'nose-parametrized'")
+        raise ValueError("Please use the 'parameterized' (note the second 'e' in 'parameterized') pypi package instead of 'nose-parametrized'")
     setup(
         name="nose-parametrized",
-        version="0.0",
-        description="please use 'nose-parameterized' for installation",
+        version="0.1",
+        description="Please use the 'parameterized' package (note the second 'e' in 'parameterized')",
     )
