587af6d8899783061bc289f718c01a3f7c882720
Unknown <charlsagente@gmail.com>
2017-11-18 23:33:12 -0500

    Updated Doc and test format
    * I reformatted the last test I made and also the doc. * English is not my first language, I apologize for any inconvenience. 
    * I added green output results for my test results (I also found parameterized project because of green)

--- parameterized/test.py
+++ parameterized/test.py
@@ -70,7 +70,7 @@ class TestParameterized(object):
 
 
 if not PYTEST:
-    # py.test doesn't use xunit-style setup/teardown`, so these tests don't apply
+    # py.test doesn't use xunit-style setup/teardown, so these tests don't apply
     class TestSetupTeardown(object):
         expect("generator", [
             "test_setup(setup 1)",
@@ -323,44 +323,45 @@ def test_with_docstring(input):
     ("x, y=9, *a, **kw", param(1, 2, 3, z=3), [("x", 1), ("y", 2), ("*a", (3,)), ("**kw", {"z": 3})]),
 ])
 class TestParameterizedClass(TestCase):
-    expect(['TestParameterizedClass_1:test_method_a()',
-            'TestParameterizedClass_1:test_method_b()',
-            'TestParameterizedClass_2:test_method_a()',
-            'TestParameterizedClass_2:test_method_b()',
-            'TestParameterizedClass_3:test_method_a()',
-            'TestParameterizedClass_3:test_method_b()',
-            'TestParameterizedClass_4:test_method_a()',
-            'TestParameterizedClass_4:test_method_b()',
-            'TestParameterizedClass_5:test_method_a()',
-            'TestParameterizedClass_5:test_method_b()',
-            'TestParameterizedClass_6:test_method_a()',
-            'TestParameterizedClass_6:test_method_b()',
-            'TestParameterizedClass_7:test_method_a()',
-            'TestParameterizedClass_7:test_method_b()',
-            'TestParameterizedClass_8:test_method_a()',
-            'TestParameterizedClass_8:test_method_b()',
-            'TestParameterizedClass_9:test_method_a()',
-            'TestParameterizedClass_9:test_method_b()',
-            'TestParameterizedClass_10:test_method_a()',
-            'TestParameterizedClass_10:test_method_b()',
-            'TestParameterizedClass_11:test_method_a()',
-            'TestParameterizedClass_11:test_method_b()',
-            'TestParameterizedClass_12:test_method_a()',
-            'TestParameterizedClass_12:test_method_b()',
-            'TestParameterizedClass_13:test_method_a()',
-            'TestParameterizedClass_13:test_method_b()',
-            'TestParameterizedClass_14:test_method_a()',
-            'TestParameterizedClass_14:test_method_b()',
-            'TestParameterizedClass_15:test_method_a()',
-            'TestParameterizedClass_15:test_method_b()',
-            'TestParameterizedClass_16:test_method_a()',
-            'TestParameterizedClass_16:test_method_b()',
-            'TestParameterizedClass_17:test_method_a()',
-            'TestParameterizedClass_17:test_method_b()',
-            'TestParameterizedClass_18:test_method_a()',
-            'TestParameterizedClass_18:test_method_b()'
+    expect([
+        'TestParameterizedClass_1:test_method_a()',
+        'TestParameterizedClass_1:test_method_b()',
+        'TestParameterizedClass_2:test_method_a()',
+        'TestParameterizedClass_2:test_method_b()',
+        'TestParameterizedClass_3:test_method_a()',
+        'TestParameterizedClass_3:test_method_b()',
+        'TestParameterizedClass_4:test_method_a()',
+        'TestParameterizedClass_4:test_method_b()',
+        'TestParameterizedClass_5:test_method_a()',
+        'TestParameterizedClass_5:test_method_b()',
+        'TestParameterizedClass_6:test_method_a()',
+        'TestParameterizedClass_6:test_method_b()',
+        'TestParameterizedClass_7:test_method_a()',
+        'TestParameterizedClass_7:test_method_b()',
+        'TestParameterizedClass_8:test_method_a()',
+        'TestParameterizedClass_8:test_method_b()',
+        'TestParameterizedClass_9:test_method_a()',
+        'TestParameterizedClass_9:test_method_b()',
+        'TestParameterizedClass_10:test_method_a()',
+        'TestParameterizedClass_10:test_method_b()',
+        'TestParameterizedClass_11:test_method_a()',
+        'TestParameterizedClass_11:test_method_b()',
+        'TestParameterizedClass_12:test_method_a()',
+        'TestParameterizedClass_12:test_method_b()',
+        'TestParameterizedClass_13:test_method_a()',
+        'TestParameterizedClass_13:test_method_b()',
+        'TestParameterizedClass_14:test_method_a()',
+        'TestParameterizedClass_14:test_method_b()',
+        'TestParameterizedClass_15:test_method_a()',
+        'TestParameterizedClass_15:test_method_b()',
+        'TestParameterizedClass_16:test_method_a()',
+        'TestParameterizedClass_16:test_method_b()',
+        'TestParameterizedClass_17:test_method_a()',
+        'TestParameterizedClass_17:test_method_b()',
+        'TestParameterizedClass_18:test_method_a()',
+        'TestParameterizedClass_18:test_method_b()'
 
-            ])
+    ])
 
     def _assertions(self):
         assert hasattr(self, 'a')
